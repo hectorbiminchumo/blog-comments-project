49c9735ec70b52f55da9d8ba855ab725fea7d21b
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2023-12-30 23:03:40 -0500

    sending and handling a GET Ajax request

---
+++ public/scripts/comments.js
@@ -0,0 +1,11 @@
+const loadCommentsBtnElement = document.getElementById('load-comments-btn');
+
+async function fetchCommentsForPost() {
+    const postId = loadCommentsBtnElement.dataset.postid;
+    console.log(postId);
+    const response = await fetch(`/posts/${postId}/comments`);
+    const responseData = await response.json();
+    console.log(responseData);
+}
+
+loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
